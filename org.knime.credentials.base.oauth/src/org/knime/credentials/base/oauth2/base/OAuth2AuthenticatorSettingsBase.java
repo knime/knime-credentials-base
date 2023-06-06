39bf58c9ee41f301a9ea9c4519d72279efc8bea0
Bjoern Lohrmann <bjoern.lohrmann@knime.com>
2023-06-06 08:59:11 +0200

    AP-20546: implement OAuth2 Authenticator (Password) node
    Code is copied/adapted from the current Generic OAuth2 Authenticator.
    
    AP-20546 (OAuth2 Authenticator (Password))

---
+++ org.knime.credentials.base.oauth/src/org/knime/credentials/base/oauth2/base/OAuth2AuthenticatorSettingsBase.java
@@ -0,0 +1,100 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   2023-06-05 (bjoern): created
+ */
+package org.knime.credentials.base.oauth2.base;
+
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeSettings;
+import org.knime.core.webui.node.dialog.defaultdialog.widget.Widget;
+
+import com.github.scribejava.core.model.Verb;
+
+/**
+ * Base class for OAuth2 Authenticator settings. Defines the common settings and
+ * enums of all OAuth2 Authenticator settings classes.
+ *
+ * @author Alexander Bondaletov, Redfield SE
+ */
+@SuppressWarnings({ "restriction", "javadoc" })
+public class OAuth2AuthenticatorSettingsBase implements DefaultNodeSettings {
+
+    @Widget(title = "Token endpoint URL", description = "The token endpoint URL of the OAuth2 service.")
+    public String m_tokenUrl;
+
+    @Widget(title = "Token endpoint request method", description = "HTTP method to use when requesting the access token from the token endpoint.")
+    public HttpRequestMethod m_tokenRequestMethod = HttpRequestMethod.POST;
+
+    @Widget(title = "ID", description = "The client/application ID. In some services this is called API key.")
+    public String m_clientId;
+
+    @Widget(title = "Secret", description = "The secret for the confidential application.")
+    public String m_clientSecret;
+
+    @Widget(title = "Authentication mechanism", description = "How to transfer Client/App ID and secret to the service endpoints. HTTP Basic Auth is the most common mechanism, "
+            + "but some services expect these values to be part of the form-encoded request body.")
+    public ClientAuthenticationType m_clientAuthMechanism = ClientAuthenticationType.HTTP_BASIC_AUTH;
+
+    @Widget(title = "Scopes", description = "The list of scopes separated by the whitespace or new line.")
+    public String m_scopes;
+
+    public enum HttpRequestMethod {
+        POST, GET;
+    }
+
+    public enum ClientAuthenticationType {
+        HTTP_BASIC_AUTH, REQUEST_BODY
+    }
+
+    public enum ClientType {
+        PUBLIC, CONFIDENTIAL;
+    }
+
+    public static Verb toScribeVerb(final HttpRequestMethod method) {
+        return Verb.valueOf(method.toString());
+    }
+
+}
---
+++ org.knime.credentials.base.oauth/src/org/knime/credentials/base/oauth2/password/OAuth2AuthenticatorPasswordNodeFactory.java
@@ -0,0 +1,92 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   2023-04-13 (Alexander Bondaletov, Redfield SE): created
+ */
+package org.knime.credentials.base.oauth2.password;
+
+import org.knime.core.webui.node.impl.WebUINodeConfiguration;
+import org.knime.core.webui.node.impl.WebUINodeFactory;
+import org.knime.credentials.base.CredentialPortObject;
+
+/**
+ * The node factory for the {@link OAuth2AuthenticatorPasswordNodeModel} node.
+ *
+ * @author Alexander Bondaletov, Redfield SE
+ */
+@SuppressWarnings("restriction")
+public class OAuth2AuthenticatorPasswordNodeFactory extends WebUINodeFactory<OAuth2AuthenticatorPasswordNodeModel> {
+
+    private static final String FULL_DESCRIPTION = """
+                    <p>OAuth2 Authenticator that supports the <a href="https://oauth.net/2/grant-types/password/">resource
+                    owner password credentials (ROPC)</a> grant.
+                    </p>
+                    <p>The ROPC grant is considered legacy and does not support 2FA/MFA. Usage of this grant is
+                    discouraged and the client credentials grant should be used instead.</p>
+            """;
+
+    private static final WebUINodeConfiguration CONFIGURATION = WebUINodeConfiguration.builder()//
+            .name("OAuth2 Authenticator (Password)")//
+            .icon("")//
+            .shortDescription("OAuth2 Authenticator that supports the resource owner password credentials (ROPC) grant.")//
+            .fullDescription(FULL_DESCRIPTION)
+            .modelSettingsClass(OAuth2AuthenticatorPasswordSettings.class)//
+            .addOutputPort("Credential", CredentialPortObject.TYPE, "Credential")//
+            .sinceVersion(5, 1, 0)//
+            .build();
+
+    /**
+     * Creates new instance.
+     */
+    public OAuth2AuthenticatorPasswordNodeFactory() {
+        super(CONFIGURATION);
+    }
+
+    @Override
+    public OAuth2AuthenticatorPasswordNodeModel createNodeModel() {
+        return new OAuth2AuthenticatorPasswordNodeModel(CONFIGURATION);
+    }
+}
